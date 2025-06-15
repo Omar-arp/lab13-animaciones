4ff8c658113f91d34cd6e1dde9f7480c4a4fee55
Omar-arq <paul.delgado@tecsup.edu.pe>
2025-06-15 03:45:48 -0500

    Primer ejercicio

--- app/src/main/java/com/example/lab13animaciones/MainActivity.kt
+++ app/src/main/java/com/example/lab13animaciones/MainActivity.kt
@@ -3,26 +3,59 @@ package com.example.lab13animaciones
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.animation.*
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.material3.*
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
 import com.example.lab13animaciones.ui.theme.Lab13AnimacionesTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContent {
             Lab13AnimacionesTheme {
-
-
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    Ejercicio1AnimatedVisibility()
+                }
             }
         }
     }
 }
 
+@OptIn(ExperimentalAnimationApi::class)
+@Composable
+fun Ejercicio1AnimatedVisibility() {
+    var visible by remember { mutableStateOf(false) }
+
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(32.dp),
+        verticalArrangement = Arrangement.spacedBy(16.dp, Alignment.CenterVertically),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Button(onClick = { visible = !visible }) {
+            Text(if (visible) "Ocultar cuadro" else "Mostrar cuadro")
+        }
+
+        AnimatedVisibility(
+            visible = visible,
+            enter = fadeIn() + expandIn(),
+            exit = fadeOut() + shrinkOut()
+        ) {
+            Box(
+                modifier = Modifier
+                    .size(150.dp)
+                    .background(Color(0xFF81C784)) // Verde claro
+            )
+        }
+    }
+}
